d657ad4693291352514e4b42b67648b719ea26db
anggadirga51@gmail.com <anggarhekong@gmail.com>
2024-12-26 14:36:25 +0700

    first commit

---
+++ layout/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="float-start">
+            <p>2024 &copy; SPK - SAW Method</p>
+        </div>
+        <div class="float-end">
+            <p>Crafted with <span class="text-danger"></span> by <a
+                   >Kelompok 18</a></p>
+        </div>
+    </div>
+</footer>
